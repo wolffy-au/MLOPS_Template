7a8b6708786d1b2a85ea764f1bbb9f3f8a6083b3
wolffy-au <wolffborg1@gmail.com>
2023-11-25 16:33:09 +1100

    Moved and renamed main CICD trigger scripts

--- src/1_data_processing.py
+++ src/1_data_processing.py
@@ -5,16 +5,18 @@
 
 from data.data_loading import load_csv_data, explore_dataset
 from data.data_preprocessing import split_train_test
+from utils.algorithm_evaluation import algorithm_evaluation, compare_algorithms
 
 # Load dataset
 # url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 file_path = "data/external/iris.csv"
 dataset = load_csv_data(file_path)
 
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset.columns = names
+dataset.columns = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-explore_dataset(dataset)
+# explore_dataset(dataset)
 
-X_train, X_validation, Y_train, Y_validation = split_train_test(dataset)
+X_train, X_validation, Y_train, Y_validation = split_train_test(dataset, random_state=1)
 
+results, names = algorithm_evaluation(X_train, Y_train)
+# compare_algorithms(results, names)
