596cbb2b317e8c6d075e71d5a70965d2e7eb62df
wolffy-au <wolffborg1@gmail.com>
2023-11-25 17:41:34 +1100

    Added save, load, clear datasets to file

--- src/data/data_loading.py
+++ src/data/data_loading.py
@@ -1,8 +1,12 @@
 # Example data_loading.py
 
 import pandas as pd
+import joblib as jl
+import os
 from matplotlib import pyplot as plt
 from pandas.plotting import scatter_matrix
+
+SAVE_DATASET_PATH = "data/raw/"
 
 # Data Loading Functions: This file may contain functions for loading data from various sources, such as CSV files, databases, APIs, or other formats. The goal is to abstract away the details of data loading so that it can be easily reused throughout the project.
 def load_csv_data(file_path):
@@ -40,4 +44,17 @@ def explore_dataset(dataset):
     scatter_matrix(dataset)
     plt.show()
 
+def save_datasets(datasets, filenames, save_path=SAVE_DATASET_PATH):
+    for (dataset, filename) in zip(datasets, filenames):
+        jl.dump(dataset, f"{save_path}{filename}.dataset")
 
+def load_datasets(filenames, save_path=SAVE_DATASET_PATH):
+    datasets = []
+    for filename in filenames:
+        datasets.append(jl.load(f"{save_path}{filename}.dataset"))
+    return datasets
+
+def clear_datasets(filenames, save_path=SAVE_DATASET_PATH):
+    for filename in filenames:
+        file_path = os.path.join(f"{save_path}{filename}.dataset")
+        os.remove(file_path)
--- tests/unit/test_data_loading.py
+++ tests/unit/test_data_loading.py
@@ -1,8 +1,15 @@
 import pytest
 import pandas as pd
+import os
 import sklearn
 from sklearn.datasets import load_iris
-from src.data.data_loading import load_csv_data
+from src.data.data_loading import load_csv_data, save_datasets, load_datasets, clear_datasets
+
+# Fixture to create temporary directories
+@pytest.fixture
+def temp_dirs(tmpdir):
+    raw_dir = tmpdir.mkdir('data').mkdir('raw')
+    return str(raw_dir)
 
 # Create a sample CSV file for testing using the Iris dataset
 @pytest.fixture
@@ -41,3 +48,23 @@ def test_load_csv_data(sample_csv_file):
 # def test_load_api_data(api_endpoint):
 #     # Code to make API requests and fetch data
 #     pass
+
+def test_save_and_load_datasets(temp_dirs):
+    # Create some sample data
+    datasets = [list(range(i, i+3)) for i in range(3)]
+    filenames = ['dataset1', 'dataset2', 'dataset3']
+
+    # Test save_datasets
+    save_datasets(datasets, filenames, temp_dirs)
+
+    # Test load_datasets
+    loaded_datasets = load_datasets(filenames, temp_dirs)
+
+    # Assertions
+    assert len(loaded_datasets) == len(datasets)
+
+    for loaded, original in zip(loaded_datasets, datasets):
+        assert loaded == original
+
+    # Clean up
+    clear_datasets(filenames, temp_dirs)
