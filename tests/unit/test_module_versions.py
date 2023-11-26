fd44ee87f32f714a5749322812707b06dab7941a
wolffy-au <wolffborg1@gmail.com>
2023-11-26 19:32:06 +1100

    Switched to Poetry environment

--- tests/unit/test_module_versions.py
+++ tests/unit/test_module_versions.py
@@ -5,25 +5,25 @@ def test_python_version():
     assert 'Python' in python_version_output
 
 def test_scipy_version():
-    scipy_version_output = subprocess.check_output(['pip', 'show', 'scipy'], text=True)
+    scipy_version_output = subprocess.check_output(['poetry', 'show', 'scipy'], text=True)
     assert 'scipy' in scipy_version_output
 
 def test_numpy_version():
-    numpy_version_output = subprocess.check_output(['pip', 'show', 'numpy'], text=True)
+    numpy_version_output = subprocess.check_output(['poetry', 'show', 'numpy'], text=True)
     assert 'numpy' in numpy_version_output
 
 def test_matplotlib_version():
-    matplotlib_version_output = subprocess.check_output(['pip', 'show', 'matplotlib'], text=True)
+    matplotlib_version_output = subprocess.check_output(['poetry', 'show', 'matplotlib'], text=True)
     assert 'matplotlib' in matplotlib_version_output
 
 def test_pandas_version():
-    pandas_version_output = subprocess.check_output(['pip', 'show', 'pandas'], text=True)
+    pandas_version_output = subprocess.check_output(['poetry', 'show', 'pandas'], text=True)
     assert 'pandas' in pandas_version_output
 
 def test_sklearn_version():
-    sklearn_version_output = subprocess.check_output(['pip', 'show', 'scikit-learn'], text=True)
+    sklearn_version_output = subprocess.check_output(['poetry', 'show', 'scikit-learn'], text=True)
     assert 'scikit-learn' in sklearn_version_output
 
 def test_joblib_version():
-    joblib_version_output = subprocess.check_output(['pip', 'show', 'joblib'], text=True)
+    joblib_version_output = subprocess.check_output(['poetry', 'show', 'joblib'], text=True)
     assert 'joblib' in joblib_version_output
