cce200990604b0e218d5e4fc60a895a220c1e00d
wolffy-au <wolffborg1@gmail.com>
2023-11-26 22:17:40 +1100

    Draft evaluate model

--- src/3_evaluate.py
+++ src/3_evaluate.py
@@ -3,3 +3,14 @@
 # It may generate metrics, visualizations, or reports for model evaluation.
 # Typically executed after model training to ensure model quality before deployment.
 
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import accuracy_score
+
+# # Evaluate predictions
+# predictions = model.predict(X_validation)
+# print(accuracy_score(Y_validation, predictions))
+# print(confusion_matrix(Y_validation, predictions))
+# print(classification_report(Y_validation, predictions))
+
+evaluate_model(model, X_validation, Y_validation)
