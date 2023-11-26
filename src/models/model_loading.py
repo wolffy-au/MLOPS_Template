e35c6bd12ce39d33abacaa49702804c72eca185c
wolffy-au <wolffborg1@gmail.com>
2023-11-26 22:14:25 +1100

    Added ability to save/load single models

--- src/models/model_loading.py
+++ src/models/model_loading.py
@@ -5,18 +5,31 @@ SAVE_MODEL_PATH = os.path.normpath("data/raw/")
 
 def save_models(models, filenames, save_path=SAVE_MODEL_PATH):
     save_path = os.path.normpath(save_path)
+    if not isinstance(models, list):
+        models = [models]
+    if isinstance(filenames, str):
+        filenames = [filenames]
     for (model, filename) in zip(models, filenames):
-        jl.dump(model, f"{save_path}{filename}.model")
+        model_path = os.path.join(save_path, f"{filename}.model")
+        jl.dump(model, model_path)
 
 def load_models(filenames, save_path=SAVE_MODEL_PATH):
     save_path = os.path.normpath(save_path)
+    if isinstance(filenames, str):
+        filenames = [filenames]
     models = []
     for filename in filenames:
-        models.append(jl.load(f"{save_path}{filename}.model"))
+        model_path = os.path.join(save_path, f"{filename}.model")
+        if os.path.exists(model_path):
+            models.append(jl.load(model_path))
+            
     return models
 
 def clear_models(filenames, save_path=SAVE_MODEL_PATH):
     save_path = os.path.normpath(save_path)
+    if isinstance(filenames, str):
+        filenames = [filenames]
     for filename in filenames:
-        file_path = os.path.join(f"{save_path}{filename}.model")
-        os.remove(file_path)
+        model_path = os.path.join(save_path, f"{filename}.model")
+        if os.path.exists(model_path):
+            os.remove(model_path)
---
+++ tests/unit/test_model_loading_model.py
@@ -0,0 +1,54 @@
+import pytest
+import os
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from src.models.model_loading import save_models, load_models, clear_models
+
+# Fixture to create temporary directories
+@pytest.fixture
+def temp_dirs(tmpdir):
+    raw_dir = tmpdir.mkdir('data').mkdir('raw')
+    return str(raw_dir)
+
+# Mock data for testing
+TEST_MODEL = DecisionTreeClassifier()
+TEST_FILENAME = 'file1'
+
+@pytest.fixture
+def cleanup_after_tests(temp_dirs):
+    # Fixture to clean up files after tests
+    yield
+    clear_models(TEST_FILENAME, save_path=temp_dirs)
+
+def test_save_model(cleanup_after_tests, temp_dirs):
+    save_models(TEST_MODEL, TEST_FILENAME, save_path=temp_dirs)
+
+    # Check if files were created
+    filename = TEST_FILENAME
+    model_path = os.path.join(temp_dirs, f"{filename}.model")
+    assert os.path.exists(model_path)
+
+def test_load_model(cleanup_after_tests, temp_dirs):
+    # Save models before testing loading
+    save_models(TEST_MODEL, TEST_FILENAME, save_path=temp_dirs)
+
+    loaded_models = load_models(TEST_FILENAME, save_path=temp_dirs)
+
+    # Check if loaded models match the saved models
+    assert loaded_models[0].get_params() == TEST_MODEL.get_params()
+
+def test_clear_models(cleanup_after_tests, temp_dirs):
+    # Save models before testing clearing
+    save_models(TEST_MODEL, TEST_FILENAME, save_path=temp_dirs)
+
+    # Check if files exist before clearing
+    filename = TEST_FILENAME
+    model_path = os.path.join(temp_dirs, f"{filename}.model")
+    assert os.path.exists(model_path)
+
+    clear_models(TEST_FILENAME, save_path=temp_dirs)
+
+    # Check if files were removed after clearing
+    model_path = os.path.join(temp_dirs, f"{filename}.model")
+    assert not os.path.exists(model_path)
