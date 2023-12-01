243f046809c97305111c797a019e878768b868b8
wolffy-au <wolffborg1@gmail.com>
2023-12-01 23:39:08 +1100

    Comment out Inference

--- src/5_inference.py
+++ src/5_inference.py
@@ -3,16 +3,16 @@
 # It may include code for handling input data, calling the model, and processing the output.
 # Part of the deployment pipeline to ensure that the deployed model functions as expected.
 
-from models.model_loading import load_models
+# from models.model_loading import load_models
 
-print("Loading previous model")
-model_name = "finalised_model"
-[model] = load_models(model_name)
+# print("Loading previous model")
+# model_name = "finalised_model"
+# [model] = load_models(model_name)
 
-data = [
-    # Add sample data here
-    ]
+# data = [
+#     # Add sample data here
+#     ]
 
-for x in data:
-    # test the model with 1 row
-    print(model.predict([x]))
+# for x in data:
+#     # test the model with 1 row
+#     print(model.predict([x]))
