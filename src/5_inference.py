a94b3f8e3caf7b26d7566b7ea4c3753a63b28d2f
wolffy-au <wolffborg1@gmail.com>
2023-12-01 19:03:16 +1100

    New inference test

--- src/5_inference.py
+++ src/5_inference.py
@@ -3,20 +3,18 @@
 # It may include code for handling input data, calling the model, and processing the output.
 # Part of the deployment pipeline to ensure that the deployed model functions as expected.
 
-import pandas as pd
-
-from data.data_loading import load_datasets
 from models.model_loading import load_models
-from models.model_evaluation import evaluate_model, cross_validate_model, confusion_matrix_model, plot_confusion_matrix
-
-print("Loading datasets")
-[X_train, Y_train] = load_datasets(["X_train", "Y_train"])
 
 print("Loading previous model")
 model_name = "finalised_model"
 [model] = load_models(model_name)
 
-print("Evaluating model")
-accuracy, report = evaluate_model(model, X_train, Y_train)
-print("Accuracy score: ", accuracy, "\n")
-print("Classification report:\n", report)
+data = [
+    [7.9, 4.4, 6.9, 2.5],
+    [7.3, 3.5, 2.5, 0.2],
+    [7.3, 3.5, 2.5, 0.3],
+    ]
+
+for x in data:
+    # test the model with 1 row
+    print(model.predict([x]))
