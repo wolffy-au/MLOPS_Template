f9085eb3e93e44652e169a950d0adc89458603ff
wolffy-au <wolffborg1@gmail.com>
2023-12-10 09:08:12 +1100

    Changed module version check

--- tests/unit/test_module_versions.py
+++ tests/unit/test_module_versions.py
@@ -1,29 +1,37 @@
-import subprocess
-
-def test_python_version():
-    python_version_output = subprocess.check_output(['python', '--version'], text=True)
-    assert 'Python' in python_version_output
+import importlib
 
 def test_scipy_version():
-    scipy_version_output = subprocess.check_output(['poetry', 'show', 'scipy'], text=True)
-    assert 'scipy' in scipy_version_output
+    try:
+        importlib.import_module('scipy')
+    except ImportError:
+        assert False, 'scipy module not found'
 
 def test_numpy_version():
-    numpy_version_output = subprocess.check_output(['poetry', 'show', 'numpy'], text=True)
-    assert 'numpy' in numpy_version_output
+    try:
+        importlib.import_module('numpy')
+    except ImportError:
+        assert False, 'numpy module not found'
 
 def test_matplotlib_version():
-    matplotlib_version_output = subprocess.check_output(['poetry', 'show', 'matplotlib'], text=True)
-    assert 'matplotlib' in matplotlib_version_output
+    try:
+        importlib.import_module('matplotlib')
+    except ImportError:
+        assert False, 'matplotlib module not found'
 
 def test_pandas_version():
-    pandas_version_output = subprocess.check_output(['poetry', 'show', 'pandas'], text=True)
-    assert 'pandas' in pandas_version_output
+    try:
+        importlib.import_module('pandas')
+    except ImportError:
+        assert False, 'pandas module not found'
 
 def test_sklearn_version():
-    sklearn_version_output = subprocess.check_output(['poetry', 'show', 'scikit-learn'], text=True)
-    assert 'scikit-learn' in sklearn_version_output
+    try:
+        importlib.import_module('sklearn')
+    except ImportError:
+        assert False, 'scikit-learn module not found'
 
 def test_joblib_version():
-    joblib_version_output = subprocess.check_output(['poetry', 'show', 'joblib'], text=True)
-    assert 'joblib' in joblib_version_output
+    try:
+        importlib.import_module('joblib')
+    except ImportError:
+        assert False, 'joblib module not found'
